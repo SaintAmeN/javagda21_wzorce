48ad5becbc22c93dae0e0d358910504ec11df9fb
Paweł Recław <pawel.reclaw@gmail.com>
2019-03-24 11:14:32 +0100

    Wzorce

--- src/main/java/com/javagda21/wzorce/behavioral/observer/NewsStation.java
+++ src/main/java/com/javagda21/wzorce/behavioral/observer/NewsStation.java
@@ -1,15 +1,22 @@
 package com.javagda21.wzorce.behavioral.observer;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.StringProperty;
+
 import java.util.Observable;
 
-public class NewsStation extends Observable {
+public class NewsStation {
+
+//    private StringProperty stringProperty;
+    private SimpleObjectProperty<News> newsProperty = new SimpleObjectProperty<>();
 
     public void dodajOgladajacego(Watcher watcher){
-        addObserver(watcher);
+        newsProperty.addListener(watcher);
     }
 
     public void powiadomOWiadomosci(int waga, String wiadomosc){
-        setChanged();
-        notifyObservers(new News(waga, wiadomosc));
+//        setChanged();
+//        notifyObservers(new News(waga, wiadomosc));
+        newsProperty.setValue(new News(waga, wiadomosc));
     }
 }
--- src/main/java/com/javagda21/wzorce/behavioral/observer/Watcher.java
+++ src/main/java/com/javagda21/wzorce/behavioral/observer/Watcher.java
@@ -1,9 +1,12 @@
 package com.javagda21.wzorce.behavioral.observer;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 import java.util.Observable;
 import java.util.Observer;
 
-public class Watcher implements Observer {
+public class Watcher implements ChangeListener<News> {
     private String imie;
     private int panicLevel;
 
@@ -12,15 +15,24 @@ public class Watcher implements Observer {
         this.panicLevel = panicLevel;
     }
 
+//    @Override
+//    public void update(Observable o, Object arg) {
+//        if (arg instanceof News) {
+//            News wiadomosc = (News) arg;
+//            if (wiadomosc.getLevel() > panicLevel) {
+//                System.out.println("Wpadam w Panikę! (" + imie + ") " + wiadomosc.getMessage());
+//            } else {
+//                System.out.println("Otrzynalem (" + imie + "): " + wiadomosc.getMessage());
+//            }
+//        }
+//    }
+
     @Override
-    public void update(Observable o, Object arg) {
-        if (arg instanceof News) {
-            News wiadomosc = (News) arg;
-            if (wiadomosc.getLevel() > panicLevel) {
-                System.out.println("Wpadam w Panikę! (" + imie + ") " + wiadomosc.getMessage());
-            } else {
-                System.out.println("Otrzynalem (" + imie + "): " + wiadomosc.getMessage());
-            }
+    public void changed(ObservableValue<? extends News> observable, News oldValue, News wiadomosc) {
+        if (wiadomosc.getLevel() > panicLevel) {
+            System.out.println("Wpadam w Panikę! (" + imie + ") " + wiadomosc.getMessage());
+        } else {
+            System.out.println("Otrzynalem (" + imie + "): " + wiadomosc.getMessage());
         }
     }
 }
